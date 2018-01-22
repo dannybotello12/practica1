89d35f9f1f57d30779b7843d91c0076f36aa1f84
CEIBA\danny.botello <danny.botello@ceiba.com.co>
2018-01-22 07:50:49 -0500

    first commit

---
+++ src/app/shared/bredcrumbs/bredcrumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bredcrumbs',
+  templateUrl: './bredcrumbs.component.html',
+  styleUrls: ['./bredcrumbs.component.css']
+})
+export class BredcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
